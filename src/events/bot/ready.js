9299160bb26a95c35270f6876af64cafc7a9943c
Somma11 <killuazinFDC>
2024-10-25 12:10:28 -0300

    a

--- src/events/bot/ready.js
+++ src/events/bot/ready.js
@@ -1,13 +1,22 @@
 const {} = require("discord.js");
+const art = `
+ _____  _____ ___  ______  ___  ___       ______  _____  _   _ 
+/  ___||  _  ||  \\/  ||  \\/  | / _ \\      |  _  \\|  ___|| | | |
+\\ \`--. | | | || .  . || .  . |/ /_\\ \\     | | | || |__  | | | |
+ \`--. \\| | | || |\\/| || |\\/| ||  _  |     | | | ||  __| | | | |
+/\\__/ /\\ \\_/ /| |  | || |  | || | | |     | |/ / | |___ \\ \\_/ /
+\\____/  \\___/ \\_|  |_/\\_|  |_/\\_| |_/     |___/  \\____/  \\___/
+`;
 
 
 module.exports = {
     name:"ready",
     run:async(client) => {console.clear();
+        console.log(art);
         console.log("\n");
         console.log('\x1b[37m%s\x1b[0m', `                               > Estou online em ${client.user.username} <`);
         console.log('\x1b[37m%s\x1b[0m', `                                > Estou em ${client.guilds.cache.size}, Servidores XD <`);
-        console.log('\x1b[37m%s\x1b[0m', `                                 > discord.gg/trident <`);
+        console.log('\x1b[37m%s\x1b[0m', `                                 > https://github.com/Somma11 <`);
         console.log('\x1b[37m%s\x1b[0m', `                                 > Tenho ${client.users.cache.size} Amiguinhus :D <`);
         console.log('\x1b[37m%s\x1b[0m', `1.0.0`);
 
